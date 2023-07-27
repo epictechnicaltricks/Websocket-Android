d354cbb64a974b1a70597345bcc4fe48e0047f2c
epictechnicaltricks <78457843+epictechnicaltricks@users.noreply.github.com>
2023-07-27 23:17:21 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,47 +1,30 @@
-package com.websocket.shubhamjit;
-
-
-import android.annotation.SuppressLint;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.terminus.chatroom.Const.Const;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import ua.naiksoftware.stomp.Stomp;
 import ua.naiksoftware.stomp.StompClient;
 
 public class MainActivity extends AppCompatActivity {
-    String textfield;
-    EditText messageBox,showText;
+   
     Button send;
 
     @SuppressLint("CheckResult")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.webb);
-        send = findViewById(R.id.button352);
-        messageBox = findViewById(R.id.writemessage);
-        showText=findViewById(R.id.showText);
+        setContentView(R.layout.ws_layout);
+        send = findViewById(R.id.button1);
+      
 		
-		//code by Shubhamjit (date : 27th july 2023)
-	        // library credit : naiksoftware
+	//code by Shubhamjit (date : 27th july 2023)
+	// library credit : naiksoftware
 		
-		// Connection to ws server
+	
+	// Connection to ws server
         StompClient stompClient = Stomp.over(Stomp.ConnectionProvider.OKHTTP, "ws://your_ws_or_wss_url.com");
         Toast.makeText(this, "Start connecting to server", Toast.LENGTH_SHORT).show();
         stompClient.connect();
 		
-		// lifecycle on StompUtils.java file
+	
+	// lifecycle on StompUtils.java file
         StompUtils.lifecycle(stompClient);
 		
       
@@ -53,7 +36,9 @@ public class MainActivity extends AppCompatActivity {
             Log.i(Const.TAG, "Receive: " + stompMessage.getPayload());
             runOnUiThread(() -> {
                 try {
-                    showText.append(jsonObject.getString("message") + "\n");
+			
+                    Log.d("wsTAG",jsonObject.getString("message") + "\n");
+			
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -61,16 +46,14 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-
+        // send button on click
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                textfield = messageBox.getText().toString();
-              
-
+               
                 try {
                   
-				   // send message to server path 
+		    // send message to server path 
                     stompClient.send("/path/path_name", "your message").subscribe();
 					
                 } catch (JSONException e) {
